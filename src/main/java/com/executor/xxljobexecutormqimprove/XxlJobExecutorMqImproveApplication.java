604359cd2c1bbc28df2319d7857db69eae7e2d28
chenjian <1498377512@qq.com>
2025-06-24 23:27:23 +0800

    调通MQ，加入log日志

--- src/main/java/com/executor/xxljobexecutormqimprove/XxlJobExecutorMqImproveApplication.java
+++ src/main/java/com/executor/xxljobexecutormqimprove/XxlJobExecutorMqImproveApplication.java
@@ -1,9 +1,11 @@
 package com.executor.xxljobexecutormqimprove;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.executor.xxljobexecutormqimprove.mapper")
 public class XxlJobExecutorMqImproveApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/executor/xxljobexecutormqimprove/core/service/CommonTaskService.java
+++ src/main/java/com/executor/xxljobexecutormqimprove/core/service/CommonTaskService.java
@@ -3,5 +3,5 @@ package com.executor.xxljobexecutormqimprove.core.service;
 import com.executor.xxljobexecutormqimprove.entity.ProduceCommonTaskMessage;
 
 public interface CommonTaskService {
-    public void changeTaskInfo(ProduceCommonTaskMessage produceCommonTaskMessage);
+     void changeTaskInfo(ProduceCommonTaskMessage produceCommonTaskMessage);
 }
